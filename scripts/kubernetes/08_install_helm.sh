59e3704e573ade836a078b787a3818ac5a2aa37c
Ubuntu <ubuntu@ip-172-31-20-117.ap-southeast-1.compute.internal>
2020-04-26 06:43:45 +0000

    Update install helm script

--- scripts/kubernetes/08_install_helm.sh
+++ scripts/kubernetes/08_install_helm.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # from https://docs.aws.amazon.com/eks/latest/userguide/helm.html
+pushd /tmp
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
+popd
 
 # verify
 helm help
